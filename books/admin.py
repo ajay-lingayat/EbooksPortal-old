1fa0410bcb5b82d3ad91059642db6b5ec8b02e9f
Ajay2810-hub <lingayatajay2810@gmail.com>
2020-12-16 14:45:59 +0530

    added history admins of models to admin site

--- books/admin.py
+++ books/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
+
 from .models import *
 
+from simple_history.admin import SimpleHistoryAdmin
+
 # Register your models here.
-admin.site.register(Book)
-admin.site.register(BookSection)
+class BookHistoryAdmin(SimpleHistoryAdmin):
+    list_display = ['id', 'title', 'downloads']
+    history_list_display = ['status']
+    search_fields = ['id', 'title', 'downloads']
+
+class BookSectionHistoryAdmin(SimpleHistoryAdmin):
+    list_display = ['id', 'text']
+    history_list_display = ['status']
+    search_fields = ['id', 'text']
+
+admin.site.register(Book, BookHistoryAdmin)
+admin.site.register(BookSection, BookSectionHistoryAdmin)
--- papers/admin.py
+++ papers/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
+
 from .models import *
 
+from simple_history.admin import SimpleHistoryAdmin
+
 # Register your models here.
-admin.site.register(Paper)
-admin.site.register(PaperSection)
+class PaperHistoryAdmin(SimpleHistoryAdmin):
+    list_display = ['id', 'title', 'downloads']
+    history_list_display = ['status']
+    search_fields = ['id', 'title', 'downloads']
+
+class PaperSectionHistoryAdmin(SimpleHistoryAdmin):
+    list_display = ['id', 'text']
+    history_list_display = ['status']
+    search_fields = ['id', 'text']
+
+admin.site.register(Paper, PaperHistoryAdmin)
+admin.site.register(PaperSection, PaperSectionHistoryAdmin)
