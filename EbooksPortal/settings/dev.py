f6b8349e7e4741cac1f69ff61e0635bdd010cece
Ajay2810-hub <lingayatajay2810@gmail.com>
2021-01-12 20:15:52 +0530

    create dev.py & move rest_framework settings from base to dev

---
+++ EbooksPortal/settings/dev.py
@@ -0,0 +1,12 @@
+from .base import *
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAdminUser'
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10
+}
