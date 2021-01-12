b27cd26c43c5d18ef7c35a6fe86b4c2ea9cf0d58
Ajay2810-hub <lingayatajay2810@gmail.com>
2021-01-12 22:21:04 +0530

    renamed EbooksPortal to ebooksportal

--- EbooksPortal/asgi.py
+++ EbooksPortal/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EbooksPortal.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ebooksportal.settings')
 
 application = get_asgi_application()
