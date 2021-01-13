ff0f2bf7f6d02992ce63caeabfd4ea298be5e375
Ajay2810-hub <lingayatajay2810@gmail.com>
2021-01-13 09:41:55 +0530

    Tag model created

--- base/models.py
+++ base/models.py
@@ -3,6 +3,18 @@ from django.contrib.auth.models import *
 
 from books.models import *
 from papers.models import *
-from .models import *
+
+from simple_history.models import HistoricalRecords
 
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=32, unique=True)
+    history = HistoricalRecords()
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        db_table = 'ep_tag'
+        verbose_name = 'tag'
+        verbose_name_plural = 'tags'
