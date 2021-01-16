040eabc9fe8f6e66db658c6a0075c1e893ff72a1
Ajay2810-hub <lingayatajay2810@gmail.com>
2021-01-16 21:54:57 +0530

    booksection removed

--- books/admin.py
+++ books/admin.py
@@ -14,11 +14,4 @@ class BookHistoryAdmin(SimpleHistoryAdmin):
     date_hierarchy = 'create_date'
     filter_horizontal = ('tags',)
 
-class BookSectionHistoryAdmin(SimpleHistoryAdmin):
-    list_display = ['id', 'name']
-    history_list_display = ['status']
-    search_fields = ['id', 'name']
-    filter_horizontal = ('books',)
-
 admin.site.register(Book, BookHistoryAdmin)
-admin.site.register(BookSection, BookSectionHistoryAdmin)
--- books/models.py
+++ books/models.py
@@ -14,9 +14,7 @@ class Book(models.Model):
         max_length=200,
         default='https://github.com/Ajay2810-hub/Ebooks/raw/master/default/images/image_unavailable.jpg'
     )
-    tags = models.ManyToManyField(
-        'base.Tag', blank=True
-    )
+    tags = models.ManyToManyField('base.Tag', blank=True)
     create_date = models.DateField(auto_now_add=True)
     downloads = models.BigIntegerField(default=0)
     history = HistoricalRecords()
@@ -28,19 +26,3 @@ class Book(models.Model):
         db_table = 'ep_book'
         verbose_name = 'book'
         verbose_name_plural = 'books'
-
-class BookSection(models.Model):
-    name = models.CharField(max_length=50)
-    books = models.ManyToManyField(
-        Book,
-        blank=True
-    )
-    history = HistoricalRecords()
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        db_table = 'ep_book_section'
-        verbose_name = 'book section'
-        verbose_name_plural = 'book sections'
