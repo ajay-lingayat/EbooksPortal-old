3a9c02cb7d4aa250896ee444ea5fa7202cef5ac6
Ajay2810-hub <lingayatajay2810@gmail.com>
2021-01-18 02:21:03 +0530

    url names changed

--- books/urls.py
+++ books/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('', views.text, name='text'),
-    path('q/<str:query>', views.query, name='query'),
+    path('', views.text, name='book_text'),
+    path('q/<str:query>', views.query, name='book_query'),
     path('all', views.all_books, name='all_books'),
-    path('open-portal/<int:id_no>', views.open_portal, name='open_portal'),
-    path('mark-download', views.mark_download, name='mark_download'),
+    path('open-portal/<int:id_no>', views.open_portal, name='book_open_portal'),
+    path('mark-download', views.mark_download, name='book_mark_download'),
 ]
--- papers/urls.py
+++ papers/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('', views.text, name='text'),
-    path('q/<str:query>', views.query, name='query'),
+    path('', views.text, name='paper_text'),
+    path('q/<str:query>', views.query, name='paper_query'),
     path('all', views.all_papers, name='all_papers'),
-    path('open-portal/<int:id_no>', views.open_portal, name='open_portal'),
-    path('mark-download', views.mark_download, name='mark_download'),
+    path('open-portal/<int:id_no>', views.open_portal, name='paper_open_portal'),
+    path('mark-download', views.mark_download, name='paper_mark_download'),
 ]
