40e184d4b672e614d4d6b74a71a3e61a41944d9e
Ajay2810-hub <lingayatajay2810@gmail.com>
2021-01-18 01:57:06 +0530

    add about url

--- base/urls.py
+++ base/urls.py
@@ -5,4 +5,5 @@ urlpatterns =[
     path('', views.home, name='home'),
     path('contact', views.contact, name='contact'),
     path('profile', views.profile, name='profile'),
+    path('about', views.about, name='about'),
 ]
