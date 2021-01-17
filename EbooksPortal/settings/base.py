7cc5d041521a6cb3ae6a0938ac95be57fce04848
Ajay2810-hub <lingayatajay2810@gmail.com>
2021-01-18 02:46:45 +0530

    urls name used to navigate

--- EbooksPortal/settings/base.py
+++ EbooksPortal/settings/base.py
@@ -34,7 +34,7 @@ def get_secret(setting, secrets=SECRETS):
 SECRET_KEY = get_secret('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
