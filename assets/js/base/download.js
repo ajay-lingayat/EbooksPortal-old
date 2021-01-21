2255d718bc6ece5507707cf26d440e594e981ea7
Ajay2810-hub <lingayatajay2810@gmail.com>
2021-01-21 23:30:53 +0530

    toast styling and download redirect

--- assets/js/base/download.js
+++ assets/js/base/download.js
@@ -1,12 +1,35 @@
-function mark_download( URL ){
+function mark_download( URL, redirectURL ){
     $.ajax({
         url: URL,
         data: {},
         dataType: 'JSON',
         success: function(data){
             if ( data.ans ){
-                console.log('done!');
+                window.location.href = redirectURL;
             }
+            else {
+                snack('Something went wrong! Please try again.');
+            }
+        },
+        error: function(error) {
+            snack('Something went wrong! Please try again.');
         }
+    });
+}
+
+function snack(message) {
+    document.getElementById('toast-message').innerHTML = message;
+    $('#copied').addClass("fade");
+    $('#copied').addClass("show");
+    $('#copied').removeClass("hide");
+    setTimeout(function() {
+        var e = document.getElementById('copied');
+        e.classList.remove('show');
+        e.classList.add('hide');
+    },
+    5000);
+    $(".close").click(function() {
+        $('#copied').addClass("hide");
+        $('#copied').removeClass("show");
     });
 }
