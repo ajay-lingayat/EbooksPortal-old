05fd4f299da8c31ef0db54c9875ce2c3d2af0dc0
Ajay2810-hub <lingayatajay2810@gmail.com>
2021-01-21 22:20:51 +0530

    view functions updated

--- books/views.py
+++ books/views.py
@@ -9,108 +9,70 @@ from .models import *
 import requests
 
 # Create your views here.
-def all_books(request):
+def books(request):
     nav_actives = [None for i in range(7)]
-    nav_actives[3] = 'active'
+    text = request.GET.get('query', False)
 
-    books = Book.objects.all().order_by('create_date')
-    paginator = Paginator(
-        books,
-        per_page=12
-    )
-    page_number = request.GET.get('page', 1)
-    page = paginator.get_page(page_number)
-
-    Context = {
-        'nav_actives': nav_actives,
-        'books': page.object_list,
-        'page': page,
-    }
-    t = loader.get_template('books/all_books.html')
-    return HttpResponse(
-        t.render(
-            Context,
-            request
-        )
-    )
-
-
-def text(request):
-    if request.method == "POST":
-       txt = request.POST['q']
-       return redirect(f'/books/q/{txt}')
-    else:
-       raise Http404()
-
-def query(request, query):
-    nav_actives = [None for i in range(7)]
-
-    try:
-        query = query.strip().lower()
-    except Exception as e:
-        print(e)
-        query = False
-
-    if query:
+    if text:
+       query = text.strip().lower()
        qlookup1 = Q(tags__name__lower=query)
        qlookup2 = Q(title__lower__contains=query)
-       books = Book.objects.filter(qlookup1 | qlookup2).distinct()
+       books = Book.objects.filter(qlookup1 | qlookup2).distinct().order_by('create_date')
 
-       paginator = Paginator(
-           books.order_by('create_date'),
-           per_page=12
-       )
+       offset = int(request.GET.get('offset', 10))
        page_number = request.GET.get('page', 1)
-       page = paginator.get_page(page_number)
+       page = Paginator(books, per_page=offset).get_page(page_number)
 
        Context = {
            'nav_actives': nav_actives,
            'books': page.object_list,
            'page': page,
-           'query': query,
+           'query': text,
+           'offset': offset,
+           'limits': [10, 25, 50, 100]
        }       
        t = loader.get_template('books/query_books.html')
-       return HttpResponse(
-           t.render(
-               Context,
-               request
-           )
-       )
-
+       return HttpResponse(t.render(Context, request))
     else:
-       return redirect('/books/all')
+       return redirect('/books/all/')
+       
+def all(request):
+    nav_actives = [None for i in range(7)]
+    nav_actives[3] = 'active'
 
+    offset = int(request.GET.get('offset', 10))
+    page_number = request.GET.get('page', 1)
+    books = Book.objects.all().order_by('create_date')
+    page = Paginator(books, per_page=offset).get_page(page_number)
 
-def open_portal(request, id_no):
+    Context = {
+        'nav_actives': nav_actives,
+        'books': page.object_list,
+        'page': page,
+        'offset': offset,
+        'limits': [10, 25, 50, 100]
+    }
+    t = loader.get_template('books/all_books.html')
+    return HttpResponse(t.render(Context, request))
 
-    if Book.objects.filter(id=id_no).exists():
-        book = Book.objects.get(id=id_no)
-        
-        Context = {
-            'book': book,
-        }
+def open_portal(request, book):
+    book = Book.objects.filter(id=book)
+    if book.exists():
+        Context = {'book': book.first()}
         t = loader.get_template('books/open_portal.html')
-        return HttpResponse(
-            t.render(
-                Context,
-                request
-            )
-        )
+        return HttpResponse(t.render(Context, request))
     else:
-        raise Http404()
+        raise Http404('Book id not provided!')
 
-
-def mark_download(request):
+def download(request, book):
     try:
-        book_id = request.GET.get('bk_id', False)
-        ans = False
-        if book_id:
-            book = Book.objects.get(id=book_id)
+        if book:
+            book = Book.objects.get(id=book)
             book.downloads += 1
             book.save()
-            ans = True
-        return JsonResponse({'ans': ans})
+            return JsonResponse({'ans': True})
+        return JsonResponse({'ans': False})
     except Exception as e:
         print('Error incrementing book download')
         print(e)
-        return JsonResponse({'ans':False})
+        return JsonResponse({'ans': False, 'error': e})
--- papers/views.py
+++ papers/views.py
@@ -7,103 +7,70 @@ import requests
 from django.core.paginator import Paginator
 
 # Create your views here.
-def all_papers(request):
+def papers(request, query):
     nav_actives = [None for i in range(7)]
-    nav_actives[4] = 'active'
+    text = request.GET.get('query', False)
 
-    papers = Paper.objects.all().order_by('create_date')
-
-    paginator = Paginator(
-        papers,
-        per_page=12
-    )
-    page_number = request.GET.get('page', 1)
-    page = paginator.get_page(page_number)
-
-    Context = {
-        'nav_actives': nav_actives,
-        'papers': page.object_list,
-        'page': page,
-    }
-    t = loader.get_template('papers/all_papers.html')
-    return HttpResponse(
-        t.render(
-            Context,
-            request
-        )
-    )
-
-
-def text(request):
-    if request.method == "POST":
-       txt = request.POST['q']
-       return redirect(f'/papers/q/{txt}')
-    else:
-       raise Http404()
-
-def query(request, query):
-    nav_actives = [None for i in range(7)]
-
-    try:
-        query = query.strip().lower()
-    except Exception as e:
-        print(e)
-        query = False
-
-    if query:
+    if text:
+       query = text.strip().lower()
        qlookup1 = Q(tags__name__lower=query)
        qlookup2 = Q(title__lower__contains=query)
-       papers = Paper.objects.filter(qlookup1 | qlookup2).distinct()
+       papers = Paper.objects.filter(qlookup1 | qlookup2).distinct().order_by('create_date')
 
-       paginator = Paginator(
-           papers.order_by('create_date'),
-           per_page=12
-       )
+       offset = int(request.GET.get('offset', 10))
        page_number = request.GET.get('page', 1)
-       page = paginator.get_page(page_number)
+       page = Paginator(papers, per_page=offset).get_page(page_number)
 
        Context = {
            'nav_actives': nav_actives,
            'papers': page.object_list,
            'page': page,
-           'query': query,
+           'query': text,
+           'offset': offset,
+           'limits': [10, 25, 50, 100]
        }       
        t = loader.get_template('papers/query_papers.html')
-       return HttpResponse(
-           t.render(
-               Context,
-               request
-           )
-       )
-
+       return HttpResponse(t.render(Context, request))
     else:
        return redirect('/papers/all')
 
+def all(request):
+    nav_actives = [None for i in range(7)]
+    nav_actives[4] = 'active'
 
-def open_portal(request, id_no):
-    if Paper.objects.filter(id=id_no).exists():
-        Context = { 'paper': Paper.objects.get(id=id_no) }
+    papers = Paper.objects.all().order_by('create_date')
+    offset = int(request.GET.get('offset', 10))
+    page_number = request.GET.get('page', 1)
+    page = Paginator(papers, per_page=offset).get_page(page_number)
+
+    Context = {
+        'nav_actives': nav_actives,
+        'papers': page.object_list,
+        'page': page,
+        'offset': offset,
+        'limits': [10, 25, 50, 100]
+    }
+    t = loader.get_template('papers/all_papers.html')
+    return HttpResponse(t.render(Context, request))
+
+def open_portal(request, paper):
+    paper = Paper.objects.filter(id=paper)
+    if paper.exists():
+        Context = { 'paper': paper.first() }
         t = loader.get_template('papers/open_portal.html')
-        return HttpResponse(
-            t.render(
-                Context,
-                request
-            )
-        )
+        return HttpResponse(t.render(Context, request))
     else:
-       raise Http404()
+       raise Http404('Paper id not provided!')
 
-def mark_download(request):
+def download(request, paper):
     try:
-        paper_id = request.GET.get('paper_id', False)
-        ans = False
-        if paper_id:
-            paper = paper.objects.get(id=paper_id)
+        if paper:
+            paper = paper.objects.get(id=paper)
             paper.downloads += 1
             paper.save()
-            ans = True
-        return JsonResponse({'ans':ans})
+            return JsonResponse({'ans': True})
+        return JsonResponse({'ans': False})
     except Exception as e:
         print('Error incrementing book download')
         print(e)
-        return JsonResponse({'ans':False})
+        return JsonResponse({'ans': False, 'error': e})
