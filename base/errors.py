3017ee8f7b0e00255bfe4c35e270970c8dde4637
Ajay2810-hub <lingayatajay2810@gmail.com>
2021-01-22 18:07:16 +0530

    error view update

--- base/errors.py
+++ base/errors.py
@@ -2,30 +2,17 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 
-
 def error403(request, exception):
     t = loader.get_template('errors/403.html')
-    return HttpResponse(
-        t.render(
-            {},
-            request
-        )
-    )
+    data = {'exception', exception}
+    return HttpResponse(t.render(data, request))
 
 def error404(request, exception):
     t = loader.get_template('errors/404.html')
-    return HttpResponse(
-        t.render(
-            {},
-            request
-        )
-    )
+    data = {'exception', exception}
+    return HttpResponse(t.render(data, request))
 
-def error500(request):
+def error500(request, exception):
     t = loader.get_template('errors/500.html')
-    return HttpResponse(
-        t.render(
-            {},
-            request
-        )
-    )
+    data = {'exception', exception}
+    return HttpResponse(t.render(data, request))
