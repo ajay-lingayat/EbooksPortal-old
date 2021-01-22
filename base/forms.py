71c5a3e72829e87ec0a3439ba6d2916efc821491
Ajay2810-hub <lingayatajay2810@gmail.com>
2021-01-22 18:47:08 +0530

    update profile & contactforms

--- base/forms.py
+++ base/forms.py
@@ -2,9 +2,7 @@ from django import forms
 from django.forms import TextInput, Textarea
 
 from allauth.account.forms import SignupForm
-
 from captcha.fields import ReCaptchaField
-
 
 class ContactForm(forms.Form):
     firstname = forms.CharField(max_length=15, widget=TextInput())
@@ -13,73 +11,19 @@ class ContactForm(forms.Form):
     subject = forms.CharField(max_length=50, widget=TextInput())
     message = forms.CharField(max_length=500, widget=Textarea())
 
-
 class CustomSignUpForm(SignupForm):
-    first_name = forms.CharField(
-        max_length=15,
-        widget=forms.TextInput(
-            attrs={
-                'placeholder': 'Firstname',
-            }
-        )
-    )
-    last_name = forms.CharField(
-        max_length=15,
-        widget=forms.TextInput(
-            attrs={
-                'placeholder': 'Lastname',
-            }
-        )
-    )
+    first_name = forms.CharField(max_length=15, widget=forms.TextInput(attrs={'placeholder': 'Firstname',}))
+    last_name = forms.CharField(max_length=15, widget=forms.TextInput(attrs={'placeholder': 'Lastname',}))
     captcha = ReCaptchaField()
 
-    def save(self, request):
-        user = super(CustomSignupForm, self).save(request)
+    def signup(self, request, user):
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.save()
         return user
 
-
-
 class ProfileForm(forms.Form):
-    firstname = forms.CharField(
-        max_length=15,
-        widget=TextInput(
-            attrs={
-                'placeholder': 'Firstname',
-                'autocomplete': 'off',
-                'required': '',
-                'class': 'form-control',
-            }
-        )
-    )
-    lastname = forms.CharField(
-        max_length=15,
-        widget=TextInput(
-            attrs={
-                'placeholder': 'Lastname',
-                'autocomplete': 'off',
-                'required': '',
-                'class': 'form-control',
-            }
-        )
-    )
-    email = forms.EmailField(
-        widget=TextInput(
-            attrs={
-                'placeholder': 'Email',
-                'autocomplete': 'off',
-                'required': '',
-                'class': 'form-control',
-            }
-        )
-    )
-    username = forms.CharField(
-        max_length=15,
-        widget=TextInput(
-            attrs={
-                'placeholder': 'Username',
-                'autocomplete': 'off',
-                'required': '',
-                'class': 'form-control',
-            }
-        )
-    )
+    firstname = forms.CharField(max_length=15, widget=TextInput())
+    lastname = forms.CharField(max_length=15, widget=TextInput())
+    email = forms.EmailField(widget=TextInput())
+    username = forms.CharField(max_length=15, widget=TextInput())
